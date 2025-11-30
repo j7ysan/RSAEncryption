f0090bc1e22b0b68f7984055735d02dcc5393a25
j2ysan <jeszaree@gmail.com>
2025-11-30 03:10:36 -0800

    sampling Lab code to shape into our future desired RSA encryption code

---
+++ main.py
@@ -0,0 +1,35 @@
+import argparse
+from key_manager import (
+    generate_keys,
+    encrypt_file,
+    decrypt_file,
+    wrap_data_key_with_rsa,
+)
+
+def main():
+    parser = argparse.ArgumentParser(description="Lab 2: Key Management Toolkit")
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    subparsers.add_parser("gen-keys", help="Generate RSA keys and initial data key")
+    enc = subparsers.add_parser("encrypt", help="Encrypt sample data with current data key")
+    enc.add_argument("--infile", default="data/plain.txt", help="Input file to encrypt")
+    enc.add_argument("--outfile", default="data/cipher.bin", help="Output encrypted file")
+    dec = subparsers.add_parser("decrypt", help="Decrypt sample data with current data key")
+    dec.add_argument("--infile", default="data/cipher.bin", help="Encrypted file to decrypt")
+    dec.add_argument("--outfile", default="data/decrypted.txt", help="Output plaintext file")
+    subparsers.add_parser("wrap", help="Wrap (encrypt) current data key with RSA public key")
+    args = parser.parse_args()
+
+    if args.command == "gen-keys":
+        generate_keys()
+    elif args.command == "encrypt":
+        encrypt_file(args.infile, args.outfile)
+    elif args.command == "decrypt":
+        decrypt_file(args.infile, args.outfile)
+    elif args.command == "wrap":
+        wrap_data_key_with_rsa()
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
